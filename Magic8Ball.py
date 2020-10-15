75df7f59b431e19929187b3a6003085816d6dbab
Michael Stringer Jr <michael.stringer.2.0@gmail.com>
2020-10-15 10:19:46 -0500

    First draft - 3 lines of code!

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,9 +1,11 @@
 #Your code goes here
 # remember to document the code for others and consider the readability of your code. 
-
+import random
 
 # Store all of the 8-ball's possible responses (shown below) in a list
 
 
 # Have the program prompt the user to ask the magic 8-ball a question
 # then return and print a random response.
+input("Ask the magic 8-ball a question: ")
+print("You shake the virutal 8-ball and it shows....\n ",random.choice(["Ask again","Outlook good","Outlook not so good","Maybe","I'm not sure","Come back tomorrow","Really?","I don't want to say"]))
